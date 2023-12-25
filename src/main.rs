caf93364ddaea50fcb5d5d6bd9343912caa228c0
Fawad Ahmed <iam.skipperod@gmail.com>
2023-12-25 12:23:36 +0500

    feat(for-loop): add for loop syntax

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello, world!");
+    print_with_for_loop_n_times(255)
+}
+
+fn print_with_for_loop_n_times(number: u8) {
+    let mut count = 0;
+    while count < number {
+        println!("count: {}", count);
+        count = count + 1;
+    }
+}
